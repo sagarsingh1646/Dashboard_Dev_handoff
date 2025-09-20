866561f03f4dcc301841170204573df96b6bd3a2
Sagar Singh <sagarsingh@Sagars-MacBook-Pro.local>
2025-09-20 20:28:35 +0530

    Ecommerce import fixed Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@
 
 import { User } from '../data/userData';
 import TopBar from '../components/layout/TopBar';
-import Ecommerce from '../components/layout/eCommerce';
+import Ecommerce from '../components/layout/Ecommerce';
 import LeftSidebar from '../components/layout/LeftSidebar';
 import RightSideBar from '../components/layout/RightSideBar';
 
